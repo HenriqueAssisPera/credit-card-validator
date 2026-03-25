2e6ac83489784220af10e7d386f139bf9e6ee9f7
Henrique Assis <henrique.assis@dnx.tec.br>
2026-03-24 22:58:28 -0300

    Tests: Refactors validation and expands card testing;

--- src/CreditCardValidator/Validators/CreditCardValidator.cs
+++ src/CreditCardValidator/Validators/CreditCardValidator.cs
@@ -4,10 +4,23 @@ public class CardValidator
 {
     public CreditCardValidationResult Validate(string cardNumber)
     {
-        return new CreditCardValidationResult
+        #region null check and trim
+        if (string.IsNullOrWhiteSpace(cardNumber))
+            return new CreditCardValidationResult { IsValid = false, Brand = "UNKNOW" };
+
+        cardNumber = cardNumber.Trim();
+        #endregion
+
+        if (cardNumber.StartsWith("4") && cardNumber.Length == 16)
         {
-            IsValid = false,
-            Brand = "UNKNOWN"
-        };
+            return new CreditCardValidationResult { IsValid = true, Brand = "VISA" };
+        }
+        else if (cardNumber.StartsWith("4"))
+        {
+            return new CreditCardValidationResult { IsValid = false, Brand = "VISA" };
+        }
+
+        return new CreditCardValidationResult { IsValid = false, Brand = "UNKNOW" };
+
     }
 }
--- tests/CreditCardValidator/Validators/CreditCardValidatorTests.cs
+++ tests/CreditCardValidator/Validators/CreditCardValidatorTests.cs
@@ -6,14 +6,22 @@ namespace CreditCardValidator.UnitTests;
 
 public class CreditCardValidatorTests
 {
-    [Fact]
-    public void Should_validate_visa_card_as_valid()
+    [Theory]
+    [InlineData("4111111111111111", "VISA", true)]
+    [InlineData("4111111111111", "VISA", false)]
+    [InlineData("4012888888881881", "VISA", true)]
+    [InlineData("378282246310005", "AMEX", true)]
+    [InlineData("6011111111111117", "DISCOVER", true)]
+    [InlineData("5105105105105100", "MASTERCARD", true)]
+    [InlineData("5105105105105106", "MASTERCARD", false)]
+    [InlineData("9111111111111111", "UNKNOWN", false)]
+    public void Should_validate_cards_correctly(string cardNumber, string expectedBrand, bool expectedIsValid)
     {
         var validator = new CardValidator();
 
-        var result = validator.Validate("4111111111111111");
+        var result = validator.Validate(cardNumber);
 
-        result.IsValid.Should().BeTrue();
-        result.Brand.Should().Be("VISA");
+        result.Brand.Should().Be(expectedBrand);
+        result.IsValid.Should().Be(expectedIsValid);
     }
 }
