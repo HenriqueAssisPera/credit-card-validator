e62c3a1bc950a1192146cd0dff60451aa0572c78
Henrique Assis <henrique.assis@dnx.tec.br>
2026-03-29 15:28:50 -0300

    refactor: improve validation coverage and test

--- src/CreditCardValidator/Features/Card/RegisterCardCommandValidator.cs
+++ src/CreditCardValidator/Features/Card/RegisterCardCommandValidator.cs
@@ -7,13 +7,18 @@ namespace CreditCardValidator.Features.RegisterCard
         public RegisterCardCommandValidator()
         {
             RuleFor(x => x.CardNumber)
-                .NotEmpty().WithMessage("Card number is required");
+                .NotEmpty().WithMessage("Card number is required.")
+                .Matches(@"^\d+$").WithMessage("Card number must contain only numeric characters.");
 
             RuleFor(x => x.FullName)
-                .NotEmpty().WithMessage("Full name is required");
+                .NotEmpty().WithMessage("Full name is required.");
 
             RuleFor(x => x.BirthDate)
-                .NotEmpty().WithMessage("Birth date is required");
+                .NotEmpty().WithMessage("Birth date is required.");
+
+            RuleFor(x => x.BirthDate)
+                .LessThan(DateTime.Today).WithMessage("Birth date must not be a future date.")
+                .When(x => x.BirthDate.HasValue);
         }
     }
 }
